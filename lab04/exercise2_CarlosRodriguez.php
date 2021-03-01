751b188da851815e2457ae9f33200c8235fdab45
Carlos <carrod@ttu.ee>
2021-03-01 16:06:14 +0200

    exercise 2 second commit

--- lab04/exercise2_CarlosRodriguez.php
+++ lab04/exercise2_CarlosRodriguez.php
@@ -1,8 +1,26 @@
 <?php
 include "./incl/phonics.php";
 $file = './incl/data/text.txt';
+$handle = fopen($file, "r");
+//echo fread($handle,filesize($file));
+echo fread($handle, 1);
+echo filesize($file);
 
-try{
+$i=1;
+$count =0;
+$newArrayCountVowels=array();
+for ($i=1; $i<filesize($file);$i++) {
+    foreach ($vowels as $valores){
+        if (fread($handle,$i)==$valores){
+            $newArrayCountVowels[$valores]=++$count;
+        }
+    }
+++$i;
+fseek($handle,$i);
+}
+
+print_r($newArrayCountVowels);
+/*try{
     if (file_exists($file)) { 
         $handle = fopen($file, "r");
         print("thats ok");
@@ -14,7 +32,7 @@ try{
     } 
     catch (Exception $e) {
     echo 'Caught exception: ', $e->getMessage(), "\n";
-}
+}*/
 
 
 
