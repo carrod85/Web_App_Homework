90c7568384fb3ef620158f3e11e3f615b49595f0
Carlos <carrod@ttu.ee>
2021-03-01 18:04:19 +0200

    saving 2nd exercise

--- lab04/exercise2_CarlosRodriguez.php
+++ lab04/exercise2_CarlosRodriguez.php
@@ -1,39 +1,25 @@
 <?php
 include "./incl/phonics.php";
 $file = './incl/data/text.txt';
-$handle = fopen($file, "r");
+$handle = fopen($file, "r") or die("Unable to open");
 //echo fread($handle,filesize($file));
-echo fread($handle, 1);
-echo filesize($file);
 
-$i=1;
-$count =0;
+
 $newArrayCountVowels=array();
-for ($i=1; $i<filesize($file);$i++) {
+$count =0;
+
+while(!feof($handle)) {
+    $cmp = fgetc($handle);
     foreach ($vowels as $valores){
-        if (fread($handle,$i)==$valores){
+        $val =strcmp($cmp , $valores);
+        if($val == 0) {
             $newArrayCountVowels[$valores]=++$count;
+
+        }
+        else{
+            continue;
         }
     }
-++$i;
-fseek($handle,$i);
 }
-
 print_r($newArrayCountVowels);
-/*try{
-    if (file_exists($file)) { 
-        $handle = fopen($file, "r");
-        print("thats ok");
-        echo fread($handle,filesize($file));
-// operate with the file contents
-        fclose($handle); } 
-    else {
-        throw new Exception("File not found!"); }
-    } 
-    catch (Exception $e) {
-    echo 'Caught exception: ', $e->getMessage(), "\n";
-}*/
-
-
-
 ?>
