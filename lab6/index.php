f9ab22310838238b913436b99f95613a71d3da54
carrod <carrod@ttu.ee>
2021-03-23 16:58:17 +0200

    modification of index and template

--- lab6/index.php
+++ lab6/index.php
@@ -7,25 +7,11 @@ $t = new Template(TEMPLATE_PATH."/index_tpl.php");
 
 $tableHead = ["code", "name", "points", "semester"];
 // Generate form
-$form = '<div class ="proba">
-<form action="index.php" method="POST">
-<div class ="code">
-<legend>Course Code(first 3 digits or I00):</legend>
-<input id="code" type="text" name="code" placeholder="I00 or ICA/ICD/etc." maxlength="3" required="">
-</div>
-<div class="boton">
-<input class="boton" type="submit" name ="submit" id="search" value="Search">
-</div>
-</form>
-<form action="index.php" method="POST">
-<div class="boton2">
-<input class="boton" type="submit" id="reset" name="reset" value="Reset">
-</div>
-</form>'; // only fill it if you use the form
+ // only fill it if you use the form
 // Assign values
 $t -> assign("title", "Courses");
 
-$t -> assign("form", $form); // fill this if you use a form
+ // fill this if you use a form
 $t -> assignTable("table", getCoursesTable(), $tableHead);
 
 // Render content echo 
